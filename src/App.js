1039e54145b039c092609c98e42963ff7eccde0a
KilHyun-Kim <rlaarlfgus@gmail.com>
2021-05-01 16:59:25 +0900

    Setting before side-project start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import { Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route component={MainPage} path={'/'} exact />
+      <Route component={AboutPage} path={'/about'} exact />
+    </>
   );
 }
 
